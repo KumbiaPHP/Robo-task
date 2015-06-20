4f29ef8db1f1aa6abb6929fb8485b52a0e436a4f
Nelson Rojas <nelson.rojas.n@gmail.com>
2015-06-20 01:16:06 -0300

    Se agregan tareas para crear scaffold controllers y modelos

--- RoboFile.php
+++ RoboFile.php
@@ -136,4 +136,106 @@ class RoboFile extends \Robo\Tasks
         $this->say("<error>Carpeta de Vistas ya existía en {$viewsDir}</error>");
       }
     }
+
+    /* permite crear un modelo ActiveRecord */
+    public function kumbiaCreateModel($modelName) {
+      $file = "app/models/{$modelName}.php";
+      $fs = new Filesystem();
+      if (!$fs->exists($file)) {
+        $this->say("<info>Creando Modelo</info>");
+        //crear archivo
+        $fs->touch($file);
+        //escribir template
+        $modelName = ucfirst($modelName);
+        $this->taskWriteToFile($file)
+           ->line("<?php")
+           ->line("\tclass {$modelName} extends ActiveRecord {")
+           ->line("\t\t")
+           ->line("\t}")
+           ->run();
+
+        $this->say("<info>Modelo creado en {$file}</info>");
+      } else {
+        $this->say("<error>Modelo ya existía en {$file}</error>");
+      }
+    }
+
+    /* permite crear un controllador scaffold para un modelo particular */
+    public function kumbiaCreateScaffoldController($controllerName, $modelName) {
+      $file = "app/controllers/{$controllerName}_controller.php";
+      $fs = new Filesystem();
+      if (!$fs->exists($file)) {
+        $this->say("<info>Creando Controlador</info>");
+        //crear archivo
+        $fs->touch($file);
+        //escribir template
+        $controllerName = ucfirst($controllerName);
+        $this->taskWriteToFile($file)
+           ->line("<?php")
+           ->line("\tclass {$controllerName}Controller extends ScaffoldController {")
+           ->line("\t\tpublic $model = "{$modelName}";")
+           ->line("\t}")
+           ->run();
+
+        $this->say("<info>Controlador creado en {$file}</info>");
+      } else {
+        $this->say("<error>Controlador ya existía en {$file}</error>");
+      }
+      //verificar existencia del modelo, y en caso que no exista crearlo
+      $modelFile = "app/models/{$modelName}.php";
+      if (!$fs->exists($modelFile)) {
+        $this->kumbiaCreateModel($modelName);
+      }
+    }
+
+    /* permite crear un modelo ActiveRecord */
+  public function kumbiaCreateModel($modelName) {
+    $file = "app/models/{$modelName}.php";
+    $fs = new Filesystem();
+    if (!$fs->exists($file)) {
+      $this->say("<info>Creando Modelo</info>");
+      //crear archivo
+      $fs->touch($file);
+      //escribir template
+      $modelName = ucfirst($modelName);
+      $this->taskWriteToFile($file)
+         ->line("<?php")
+         ->line("\tclass {$modelName} extends ActiveRecord {")
+         ->line("\t\t")
+         ->line("\t}")
+         ->run();
+
+      $this->say("<info>Modelo creado en {$file}</info>");
+    } else {
+      $this->say("<error>Modelo ya existía en {$file}</error>");
+    }
+  }
+
+  /* permite crear un controllador scaffold para un modelo particular */
+  public function kumbiaCreateScaffoldController($controllerName, $modelName) {
+    $file = "app/controllers/{$controllerName}_controller.php";
+    $fs = new Filesystem();
+    if (!$fs->exists($file)) {
+      $this->say("<info>Creando Controlador</info>");
+      //crear archivo
+      $fs->touch($file);
+      //escribir template
+      $controllerName = ucfirst($controllerName);
+      $this->taskWriteToFile($file)
+         ->line("<?php")
+         ->line("\tclass {$controllerName}Controller extends ScaffoldController {")
+         ->line("\t\t" . 'public $model = "' . $modelName . '";')
+         ->line("\t}")
+         ->run();
+
+      $this->say("<info>Controlador creado en {$file}</info>");
+    } else {
+      $this->say("<error>Controlador ya existía en {$file}</error>");
+    }
+    //verificar existencia del modelo, y en caso que no exista crearlo
+    $modelFile = "app/models/{$modelName}.php";
+    if (!$fs->exists($modelFile)) {
+      $this->kumbiaCreateModel($modelName);
+    }
+  }
 }
